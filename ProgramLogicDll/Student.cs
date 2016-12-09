9a1b8e29aafd7b91156ac09ef9707d9d817c181f
Restart <roman-skeba@mail.ru>
2016-12-09 23:41:00 +0200

    Update Student.cs

--- ProgramLogicDll/Student.cs
+++ ProgramLogicDll/Student.cs
@@ -12,9 +12,9 @@ namespace ProgramLogicDll
         public int Age { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        public string Profession { get; set; }
         public DateTime DateOfReceipt { get; set; }
         public DateTime ExpirationDate { get; set; }
-        public int GroupId { get; set; }
         public virtual Group Groups { get; set; }
     }
 }
