e2418cf66e09e4b210cb513ae7a3eaa0ccd5db6e
andriyShala <andriy.shala@i.ua>
2016-12-09 17:29:32 +0200

    update student

---
+++ UIApplication/Generate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProgramLogicDll;
+namespace UIApplication
+{
+   public static class Generate
+    {
+        public static void DistributionOfStudents(ConnectDb db)
+        {
+            db.Students
+            foreach (var item in )
+            {
+
+            } 
+
+        }
+    }
+}
