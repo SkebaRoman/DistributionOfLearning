b2ddc81c0dd52b6aa18debb565bbd47fb1f7668a
Restart <roman-skeba@mail.ru>
2016-12-09 16:09:39 +0200

    Update

--- UIApplication/FormAddPC.cs
+++ UIApplication/FormAddPC.cs
@@ -20,14 +20,14 @@ namespace UIApplication
 
         private void FormAddPC_Load(object sender, EventArgs e)
         {
-           
+
             foreach (var item in db.Softwares.ToList())
                 listBox1.Items.Add(item);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(textBox1.Text!=string.Empty&&textBox2.Text!=string.Empty)
+            if (textBox1.Text != string.Empty && textBox2.Text != string.Empty)
             {
                 if (listBox1.SelectedIndex == -1)
                 {
@@ -37,7 +37,7 @@ namespace UIApplication
                 else
                 {
                     db.Computers.Add(new Computer { GraphicsCard = textBox2.Text, Processor = textBox1.Text, RAM = (int)numericUpDown1.Value, Softwares = new List<Software>() { db.Softwares.SingleOrDefault(x => x.Name == listBox1.SelectedItem.ToString()) }, Audiences = null });
-                        }
+                }
 
             }
             db.SaveChanges();
