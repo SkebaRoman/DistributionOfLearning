85982ebccc30398cd438598e2f9c03610136f19d
Restart <roman-skeba@mail.ru>
2016-12-09 16:53:01 +0200

    Add SoftwareAdd form

---
+++ UIApplication/FormAddSoftware.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIApplication
+{
+    public partial class FormAddSoftware : Form
+    {
+        ProgramLogicDll.ConnectDb dataBase = new ProgramLogicDll.ConnectDb();
+        public FormAddSoftware()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataBase.Softwares.Add(new ProgramLogicDll.Software { Name = textBox1.Text });
+            dataBase.SaveChanges();
+        }
+    }
+}
